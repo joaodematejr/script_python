b5a30449acc02d37f8d26edf41875046605231a4
joaodematejr <joaodematejr@gmail.com>
2020-08-12 20:09:46 -0300

    Atualização 05

--- python/05.py
+++ python/05.py
@@ -1,5 +1,13 @@
+
+class Validar:
+    def validar_numero(self, numero1):
+        if numero1 >= 0:
+            print("positivo")
+        else:
+            print("negativo")
+
+
+validar = Validar()
 numero = int(input("Digite um numero: "))
-if numero >= 0:
-    print("positivo")
-else:
-    print("negativo")
+
+validar.validar_numero(numero)
