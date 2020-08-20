beea55b15f46dd73ea45143c15cc84150c1fb447
Bruno Ibias <brunoibias@gmail.com>
2020-08-19 21:23:45 -0300

    comentarios no codigo 2

--- python/02.py
+++ python/02.py
@@ -1,16 +1,18 @@
+#mostra a mensagem que sera inputada
 print('Digite um número de 1 a 5:')
-numbero = int(input())
-if numbero == 0:
+numbero = int(input()) #variavel que pede a entrada de um numero inteiro #
+# condiçao 
+if numbero == 0: #Se o numero for 0  mostrara a mensagem
     print("Numero 0")
-elif numbero == 1:
+elif numbero == 1:#Se o numero for 1  mostrara a mensagem
     print("Numero 1")
-elif numbero == 2:
+elif numbero == 2:#Se o numero for 2  mostrara a mensagem
     print("Numero 2")
-elif numbero == 3:
+elif numbero == 3:#Se o numero for 3  mostrara a mensagem
     print("Numero 3")
-elif numbero == 4:
+elif numbero == 4:#Se o numero for 4  mostrara a mensagem
     print("Numero 4")
-elif numbero == 5:
+elif numbero == 5:#Se o numero for 5  mostrara a mensagem
     print("Numero 5")
-else:
+else: #Se o numero for deferente de 1 a 5  mostrara a mensagem
     print("Numero Invalido")
